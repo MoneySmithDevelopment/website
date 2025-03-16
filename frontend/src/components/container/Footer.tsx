c124a6d704184c9865e5955fb85c3e82df34fd2e
Josiah Elias <josiahelias11@gmail.com>
2025-03-16 23:57:18 +0100

    feat: footer design done

--- frontend/src/components/container/Footer.tsx
+++ frontend/src/components/container/Footer.tsx
@@ -1,7 +1,85 @@
+import { Tooltip, Typography } from "@mui/material";
 import React from "react";
+import moneysmith_logo from "../../logos/moneysmith_name_logo.png";
+import instagram_logo from "../../logos/instagram.png";
+import twitter_logo from "../../logos/twitter.png";
+import linkedin_logo from "../../logos/linkedin.png";
+import google_play from "../../assets/googleplay.png";
+import app_store from "../../assets/appstore.png";
 
 const Footer = () => {
-  return <div className="bg-[#121B2B] text-white">Footer</div>;
+  return (
+    <div className="bg-[#121B2B] text-white flex justify-between px-[80px] py-[40px]">
+      <div className="flex flex-col gap-[40px] font-nunito">
+        <img
+          src={moneysmith_logo}
+          alt="MoneySmith logo"
+          className="w-[227px] h-[40px]"
+        />
+
+        <Typography sx={{ fontSize: "18px", color: "#98A2B3" }}>
+          A budget isn’t a punishment, it’s a game plan. Use <br /> MoneySmith’s
+          budgeting tools to set spending limits and get <br /> alerts before
+          you go overboard.
+        </Typography>
+
+        <Typography sx={{ fontSize: "17px", color: "#D0D3D9" }}>
+          2025 © All rights reserved, Moneysmith
+        </Typography>
+      </div>
+
+      <div className="pt-[3%] flex flex-col gap-[24px] font-nunito">
+        <Typography>Company</Typography>
+
+        <ul className="text-[#D0D3D9] flex flex-col gap-[12px]">
+          <li>About</li>
+          <li>Support</li>
+          <li>FAQs</li>
+          <li>Blog</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+
+      <div className="pt-[3%] flex flex-col gap-[24px] font-nunito">
+        <Typography>Legal</Typography>
+
+        <ul className="text-[#D0D3D9] flex flex-col gap-[12px]">
+          <li>Partner with us</li>
+          <li>Privacy Policy</li>
+          <li>T&Cs</li>
+        </ul>
+      </div>
+
+      <div className="pt-[3%] flex flex-col gap-[40px] font-nunito">
+        <Typography>Connect with us</Typography>
+
+        <div className="flex gap-[10px]">
+          <div className="bg-[#FFFFFF0D] rounded-full p-[10px]">
+            <Tooltip title="LinkedIn" placement="top">
+              <img src={linkedin_logo} alt="Linkedin logo" />
+            </Tooltip>
+          </div>
+          <div className="bg-[#FFFFFF0D] rounded-full p-[10px]">
+            <Tooltip title="Instagram" placement="top">
+              <img src={instagram_logo} alt="Instagram logo" />
+            </Tooltip>
+          </div>
+
+          <div className="bg-[#FFFFFF0D] rounded-full p-[10px]">
+            <Tooltip title="Twitter" placement="top">
+              <img src={twitter_logo} alt="Twitter logo" />
+            </Tooltip>
+          </div>
+        </div>
+
+        <div className="flex gap-[12px]">
+          <img src={google_play} alt="Google play" />
+
+          <img src={app_store} alt="Apple store" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
